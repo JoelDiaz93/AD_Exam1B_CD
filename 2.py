81ff3d019d178d64bef7cac96f4a05eb5e4a8fed
Joel <cdiaz_jl@hotmail.com.ar>
2021-07-30 20:36:34 -0500

    1,2,3

---
+++ 2.py
@@ -0,0 +1,47 @@
+import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+###API ########################
+ckey = "YZZwo7GpfrQXthMk2DzUOk1pj"
+csecret = "SLVB2nDMZtM94R3k6L9BRuRleQSbr3wvt9U5HZ7Sem4geedRwk"
+atoken = "2353244641-ArSJMSLFUgalKFwgHcxGJMBKDJilDton7lFO8OJ"
+asecret = "556MIX1xYUd22D0oeVdoAO5xHY1LFFrlrs3XgIw0tRQd9"
+
+
+#####################################
+class listener(StreamListener):
+
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = db.save(dictTweet)
+            print("SAVED" + str(doc) + "=>" + str(data))
+        except:
+            print("Already exists")
+            pass
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://admin:Joel$2021*@localhost:5984/')  # ('##############')
+try:
+    db = server.create('juegosolimpicos_twitter')
+except:
+    db = server['juegosolimpicos_twitter']
+
+'''===============LOCATIONS=============='''
+
+# twitterStream.filter(locations=[132.0318291328,33.9879199779,133.2465279422,34.7129148334])
+twitterStream.filter(track=['juegos olimpicos', 'Tokio 2020'])
---
+++ 3.py
@@ -0,0 +1,54 @@
+import dictionary as dictionary
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import pandas as pd
+import bson
+from bson.raw_bson import RawBSONDocument
+from pymongo.errors import ConnectionFailure
+
+def find_2nd(string, substring):
+    return string.find(substring, string.find(substring) + 1)
+
+
+def find_1st(string, substring):
+    return string.find(substring, string.find(substring))
+
+
+response = requests.get("https://www.elcomercio.com/tag/juegos-olimpicos/")
+soup = BeautifulSoup(response.content)
+
+Contenidos = []
+Provider = []
+Duration = []
+Start_Date = []
+Offered_By = []
+No_Of_Reviews = []
+Rating = []
+ListSearch = []
+
+titulos = soup.find_all("h3")
+contenido = soup.find_all("p")
+
+'''post_course=soup.find_all("span", class_="text-1 weight-semi line-tight")
+post_provider=soup.find_all("a", class_="color-charcoal italic")'''
+
+extracted = []
+for element in contenido:
+    # print(element)
+    element = str(element)
+    limpio = str(element[find_1st(element, '>') + 1:find_2nd(element, '<')])
+
+    # print(limpio)
+    ListSearch.append({"Content": limpio.strip()})
+
+CLIENT = MongoClient("mongodb://localhost:27017/")
+try:
+    CLIENT.admin.command('ismaster')
+    print('MongoDB connection: Success')
+except ConnectionFailure as cf:
+    print('MongoDB connection: failed', cf)
+db = CLIENT["exam"]
+game = db["juegosolimpicos"]
+
+x = game.insert_many(ListSearch)
